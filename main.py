873d0ef911b72c60509f38e121e8d5f4eda83958
isabellaalfonso <alfonsoisabella@ufl.edu>
2023-10-31 18:31:11 -0400

    decode

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ def encode(password):
         encode += str(new_digit)
         return encode
 
+def decode(password):
+    decoded = []
+    for c in password:
+        decoded_pass = (int(c) - 3) % 10
+        decoded.append(str(decoded_pass))
+    return "".join(decoded)
 def main():
     while True:
         print("Menu")
